f78681076a94bc3fe3e14755079c4bab7a078b4a
karanbro555 <90977560+karanbro555@users.noreply.github.com>
2023-09-12 17:14:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from pyrogram.types import Message, CallbackQuery
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram import filters, Client, errors, enums
 from pyrogram.errors import UserNotParticipant
 from pyrogram.errors.exceptions.flood_420 import FloodWait
