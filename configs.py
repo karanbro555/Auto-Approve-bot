7e3845c112b39e81940f392d9f20e0e8413533a1
karanbro555 <90977560+karanbro555@users.noreply.github.com>
2023-09-12 16:39:57 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(getenv("API_ID", "26744850"))
     API_HASH = getenv("API_HASH", "6ec3f450bb9e9ab23f050dbafe6acbb6")
     BOT_TOKEN = getenv("BOT_TOKEN", "6547420147:AAED2CrXzH-rlrM3i56Nvl9Tvkj-QP31ppc")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
+    FSUB = getenv("FSUB", "LegendBotz")
+    CHID = int(getenv("CHID", "-1001590951427"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
